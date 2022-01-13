52ccb7a2afebc059d3d1ff59ac8c94ce751a355b
LOVENEET21103067 <97654214+LOVENEET21103067@users.noreply.github.com>
2022-01-13 18:35:47 +0530

    Add files via upload

---
+++ PYTHON_ASSIGNMENT-OUTPUT(21103067).py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: E:/PEC NOTES/assignments/PYTHON_ASSIGNMET-CODE(21103067).py =====
+#1st output
+Enter first number65
+Enter second number25
+Enter third number45
+The average of three numbers is : 45.0
+
+#2nd output
+enter your gross income75000
+Enter your number of dependents4
+Your income tax is : 10600.0
+
+#3rd output 
+Enter your SID21103198
+Enter your nameVIVEK
+Enter your Gender M
+Enter your course nameB.TECH
+Enter your CGPA8.4
+['21103198', 'VIVEK', 'M', 'B.TECH', 8.4]
+
+#4th output
+Enter marks of students64
+Enter marks of students82
+Enter marks of students73
+Enter marks of students69
+Enter marks of students53
+['53', '64', '69', '73', '82']
+
+#5th output
+Part a question :  ['Red', 'Green', 'White', 'Pink', 'Yellow']        #1st part
+Part b of question : ['Red', 'Green', 'White', 'Purple', 'Yellow']    #2nd part
