1d9f32c7ba1ad743fe1ffdf82a9da3f55306eb12
LOVENEET21103067 <97654214+LOVENEET21103067@users.noreply.github.com>
2022-01-23 00:18:42 +0530

    Add files via upload

---
+++ Assignment2_output(211030367).py
@@ -0,0 +1,44 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=================== RESTART: D:/Assignment_code(211030367).py ==================
+#1st output
+35
+egaugnal evitisnes esac a si nohtyP
+Python is a object oriented language
+10
+Pythonisacasesensitivelanguage
+
+#2nd output
+enter your nameVivek
+enter your sid2110540
+enter your departmentCSE
+enter your cgpa9.6
+Hey Vivek, Here!
+My SID is 2110540
+I am from CSE and my CGPA is 9.600000
+
+#3rd output
+8
+58
+50
+left sift a with 2 bits: 224
+left shift b with 2 bits: 40
+right shift a with 2 bits: 14
+right shift b with 4 bits: 0
+
+#4th output
+enter 1st number69
+enter 2nd number45
+enter 3rd number96
+greater number is: 96.0
+
+#5th output
+enter a stringMy name is vivek
+Yes
+
+#6th output
+enter first side of triangle5
+enter second side of triangle12
+enter third side of triangle13
+YES
