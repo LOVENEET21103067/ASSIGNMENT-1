556c65696017707887c86f6cc45451384e4d6f95
LOVENEET21103067 <97654214+LOVENEET21103067@users.noreply.github.com>
2022-01-23 00:52:44 +0530

    Update Assignment2_output(211030367).py

--- Assignment2_output(211030367).py
+++ Assignment2_output(211030367).py
@@ -1,43 +1,45 @@
 Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-=================== RESTART: D:/Assignment_code(211030367).py ==================
-#1st output
-35
-egaugnal evitisnes esac a si nohtyP
-Python is a object oriented language
-10
-Pythonisacasesensitivelanguage
+================== RESTART: D:\Assignment2_code(211030367).py ==================
+#1st OUTPUT
+35                                       #(a)
+egaugnal evitisnes esac a si nohtyP      #(b)
+a case sensitive language                #(c)
+Python is a object oriented language     #(d)
+10                                       #(e)
+Pythonisacasesensitivelanguage           #(f)
 
-#2nd output
+
+#2nd OUTPUT
 enter your nameVivek
-enter your sid2110540
+enter your sid2110351
 enter your departmentCSE
 enter your cgpa9.6
 Hey Vivek, Here!
-My SID is 2110540
+My SID is 2110351
 I am from CSE and my CGPA is 9.600000
 
-#3rd output
-8
-58
-50
-left sift a with 2 bits: 224
-left shift b with 2 bits: 40
-right shift a with 2 bits: 14
-right shift b with 4 bits: 0
+#3rd OUTPUT
+8                                         #(a)
+58                                        #(b)
+50                                        #(c)
+left sift a with 2 bits: 224              #(d)
+left shift b with 2 bits: 40              #(d)
+right shift a with 2 bits: 14             #(e)
+right shift b with 4 bits: 0              #(e)
 
-#4th output
-enter 1st number69
-enter 2nd number45
-enter 3rd number96
+#4th OUTPUT
+enter 1st number96
+enter 2nd number64
+enter 3rd number69
 greater number is: 96.0
 
-#5th output
+#5th OUTPUT
 enter a stringMy name is vivek
 Yes
 
-#6th output
+#6th OUTPUT
 enter first side of triangle5
 enter second side of triangle12
 enter third side of triangle13
