ed20bbbdb61334ce29cba7f600bea898c856e42b
LOVENEET21103067 <97654214+LOVENEET21103067@users.noreply.github.com>
2022-02-14 23:02:12 +0530

    Add files via upload

---
+++ Assignment3_output(21103067).py
@@ -0,0 +1,57 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: D:\PEC\assignment21103067.py ====================
+Give a string-python program to count the number of occurence of each word 
+{'python': 1, 'program': 1, 'to': 1, 'count': 1, 'the': 1, 'number': 1, 'of': 2, 'occurence': 1, 'each': 1, 'word': 1}
+Question 1 ends
+Enter a month:12
+Enter a day:31
+Enter a year:2012
+Date: 31 / 12 / 2012
+Next_Date: 1/1/ 2013
+Question 2 ends
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Question 3 ends
+Enter a number between 4 and 10:9
+Performance=Excellent
+Letter Grade=A
+Question 4 ends
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+Question 5 ends
+********(a)********
+Give the sid (a number):21150215
+Enter your name:rohan
+give a letter y or n:y
+Give the sid (a number):2554120
+Enter your name:henery
+give a letter y or n:n
+The required dictionary-- {21150215: 'rohan', 2554120: 'henery'}
+The list will be used in further parts--
+The required list-- [(21150215, 'rohan'), (2554120, 'henery')]
+********(b)********
+The dictionary we had-
+{21150215: 'rohan', 2554120: 'henery'}
+now we have exchanged the key value pair but its not sorted-
+{'rohan': 21150215, 'henery': 2554120}
+The unsorted list--
+[('rohan', 21150215), ('henery', 2554120)]
+The sorted list--
+[('henery', 2554120), ('rohan', 21150215)]
+The sorted dictionary-
+{'henery': 2554120, 'rohan': 21150215}
+This is the dictionary sorted on the basis of name-
+{2554120: 'henery', 21150215: 'rohan'}
+********(c)********
+The list we made before will be used now-
+[(21150215, 'rohan'), (2554120, 'henery')]
+now the list is sorted-
+[(2554120, 'henery'), (21150215, 'rohan')]
+sorted dictionary based on sid- {2554120: 'henery', 21150215: 'rohan'}
+********(d)********
+Please enter one of the sids you entered before-
