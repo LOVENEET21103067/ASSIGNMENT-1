ca0b36311f77334629d6ba7809021fe80a59234f
LOVENEET21103067 <97654214+LOVENEET21103067@users.noreply.github.com>
2022-02-26 23:56:12 +0530

    Add files via upload

---
+++ Assignment4_output(211030367).py
@@ -0,0 +1,62 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================== RESTART: D:\Assignment4_code(211030367).py ==================
+---------OUTPUT-1---------
+shifted disk 1 selected the rod A destination of rod C
+shifted disk 2 selected the rod A destination of rod B
+shifted disk 1 selected the rod C destination of rod B
+shifted disk 3 selected the rod A destination of rod C
+shifted disk 1 selected the rod B destination of rod A
+shifted disk 2 selected the rod B destination of rod C
+shifted disk 1 selected the rod A destination of rod C
+
+
+---------OUTPUT-2---------
+Enter the size of pascals triangle you want: 6
+PASCAL TRIANGLE USING LOOPS
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+
+
+
+PASCAL TRIANGLE USING RECURSSIONS
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+ 1 5 10 10 5 1 
+
+
+---------OUTPUT-3---------
+enter first no.:58
+enter second no.:6
+part-(a):Checking whether the quotient and remainder is a callable value or not
+False
+False
+(b) All the values are non zero
+(c) Filtered out numbers that are greater than 4 are : [13, 8, 9, 14, 9, 15, 10]
+(d) Set: {8, 9, 10, 13, 14, 15}
+(e) Immutable set: frozenset({8, 9, 10, 13, 14, 15})
+(f) Hash value of the maximum value from the above set: 15
+
+
+---------OUTPUT-4---------
+Object created
+The name of the student it LOVENEET and SID is 21103067.
+Object destroyed
+
+
+---------OUTPUT-5---------
+(a)The updated salary of Mehak is 70000 
+(b)Record of Viren deleted
+
+---------OUTPUT-6---------
+Enter the word: polite
+Enter a word using the exact same letters to test your friendship: police
+The letters aren't exact, friendship is fake
